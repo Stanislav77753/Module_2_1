35ac3f56380aceb94aa3eeaf96c4c65177f9ff2d
Stas <maikydyk@mail.ru@git config --global user.email maikydyk@mail.ru>
2018-09-25 23:40:26 +0300

    убрал лишнее в 4 задании

--- src/4.sql
+++ src/4.sql
@@ -2,8 +2,8 @@ ALTER TABLE projects
 ADD COLUMN cost INT NOT NULL;
 
 UPDATE projects
-INNER JOIN (SELECT  project_id AS ID, project_name AS Проект, SUM(salary) AS Сумма FROM
-    (SELECT projects.project_id,projects.project_name, developers.salary FROM developer_projects
+INNER JOIN (SELECT  project_id AS ID, SUM(salary) AS Сумма FROM
+    (SELECT projects.project_id, developers.salary FROM developer_projects
       INNER JOIN projects
         ON projects.project_id=developer_projects.project_id
       INNER JOIN developers
