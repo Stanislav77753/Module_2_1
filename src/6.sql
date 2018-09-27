1793280067a0abebd09e1e2b1e86dbe9a3bdee57
Stas <maikydyk@mail.ru@git config --global user.email maikydyk@mail.ru>
2018-09-27 23:17:21 +0300

    в 6 убрал лишнее

--- src/6.sql
+++ src/6.sql
@@ -51,7 +51,7 @@ SELECT all_res.Проект, all_res.PRICE / all_res.Count_developers AS MID_SAL
           INNER JOIN developers
             ON pd.developer_id = developers.id
             GROUP BY cp.project_id) AS res1) AS all_res
-            INNER JOIN (SELECT MIN(res1.Цена) AS PRICE, ID_Company FROM
+            INNER JOIN (SELECT MIN(res1.Цена) AS PRICE FROM
       (SELECT projects.cost AS Цена, cp.company_id AS ID_Company FROM company_projects AS cp
         INNER JOIN customer_projects AS cus_p
           ON cp.project_id = cus_p.project_id
